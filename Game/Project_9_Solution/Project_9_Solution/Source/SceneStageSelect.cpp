87768bea226700cbac91278a1eb297047a6eb824
arinWald <arnau.gonzalez.acosta@estudiantat.upc.edu>
2022-04-23 12:24:31 +0200

    Afegit el Project 9 (de classe) i inici de UI
    He afegit la solució del projecte de la última classe. Partim d'aquí.
    
    He afegit un mòdul més: SceneStageSelect.
    
    He canviat la imatge de la pantalla d'intro i la seva música. Més endevant será la mateixa imatge amb el nostre logo, el de la upc i tal.
    
    De moment les escenes son Intro --> Stage select --> Game --> Intro i aixi un loop.
    
    També he afegit la música de l'stage select i el SFX de quan selecciones l'stage.
    
    Atenció!! He canviat la mida de la finestra perquè encaixi amb les imatges nostres. La mida anterior era més gran i per això quan entres a l'escena de gameplay es veu més gran i tallat.

---
+++ Game/Project_9_Solution/Project_9_Solution/Source/SceneStageSelect.cpp
@@ -0,0 +1,61 @@
+#include "SceneStageSelect.h"
+
+#include "Application.h"
+#include "ModuleTextures.h"
+#include "ModuleRender.h"
+#include "ModuleAudio.h"
+#include "ModuleInput.h"
+#include "ModuleFadeToBlack.h"
+
+SceneStageSelect::SceneStageSelect(bool startEnabled) : Module(startEnabled)
+{
+
+}
+
+SceneStageSelect::~SceneStageSelect()
+{
+
+}
+
+// Load assets
+bool SceneStageSelect::Start()
+{
+	LOG("Loading background assets");
+
+	bool ret = true;
+
+	bgTexture = App->textures->Load("Assets/Sprites/beachStageSelect.png");
+	App->audio->PlayMusic("Assets/Music/01_Get Ready (Select Screen).ogg", 1.0f);
+	selectFx = App->audio->LoadFx("Assets/FX/Select.wav");
+
+	App->render->camera.x = 0;
+	App->render->camera.y = 0;		
+
+	return ret;
+}
+
+Update_Status SceneStageSelect::Update()
+{
+	//TESTING 
+	if (App->input->keys[SDL_SCANCODE_0] == Key_State::KEY_DOWN)
+	{
+		App->audio->PlayFx(selectFx);
+	}
+	//---TESTING
+	if (App->input->keys[SDL_SCANCODE_SPACE] == Key_State::KEY_DOWN)
+	{		
+		App->audio->PlayFx(selectFx);
+		App->fade->FadeToBlack(this, (Module*)App->sceneLevel_1, 90);
+	}
+
+	return Update_Status::UPDATE_CONTINUE;
+}
+
+// Update: draw background
+Update_Status SceneStageSelect::PostUpdate()
+{
+	// Draw everything --------------------------------------
+	App->render->Blit(bgTexture, 0, 0, NULL);
+
+	return Update_Status::UPDATE_CONTINUE;
+}
