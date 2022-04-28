5029a0f2dd9c1df8ed284a290862e80eb2484422
Joel Romero <joelromero.teke@gmail.com>
2022-04-28 19:39:36 +0200

    DEFINITIVO PA

--- Game/Project_9_Solution/Project_9_Solution/Source/SceneIntro.cpp
+++ Game/Project_9_Solution/Project_9_Solution/Source/SceneIntro.cpp
@@ -27,13 +27,7 @@ bool SceneIntro::Start()
 	bool ret = true;
 
 	bgTexture = App->textures->Load("Assets/Sprites/UI/startScreen.png");
-	introFx = App->audio->LoadFx("Assets/FX/introTitle.wav");
-	if (loop == true) {
-		App->audio->PlayFx(introFx,0);
-		loop = false;
-
-	}
-	//App->audio->PlayMusic("Assets/Music/introTitle.ogg", 0.0f);
+	App->audio->PlayMusic("Assets/Music/introTitle.ogg", 0.0f);
 
 	App->render->camera.x = 0;
 	App->render->camera.y = 0;
--- Game/Project_9_Solution/Project_9_Solution/Source/SceneIntro.h
+++ Game/Project_9_Solution/Project_9_Solution/Source/SceneIntro.h
@@ -31,7 +31,6 @@ public:
 	// The scene sprite sheet loaded into an SDL_Texture
 	SDL_Texture* bgTexture = nullptr;
 
-	uint introFx;
 	int timer;
 	bool scape;
 	bool loop = true;
